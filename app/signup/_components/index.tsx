b0603292bac37650c2f8a81179beef31e1caf9aa
tayyab030 <tayyaboff564@gmail.com>
2025-09-22 08:56:37 +0500

    setup the signup api

--- app/signup/_components/index.tsx
+++ app/signup/_components/index.tsx
@@ -3,8 +3,6 @@
 import dynamic from "next/dynamic";
 import type React from "react";
 
-import { useState } from "react";
-
 const SignupForm = dynamic(() => import("./SignupForm"));
 
 export default function SignupPage() {
